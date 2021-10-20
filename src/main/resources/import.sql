6cb4161bc61c5f817455e46446fa466082eb7ee0
Hendi Santika <hendisantika@gmail.com>
2021-10-20 08:58:35 +0700

    #19 - Add init Data sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS `event`
+(
+    `id`
+    INT
+    unsigned
+    COLLATE
+    utf8mb4_unicode_ci
+    NOT
+    NULL
+    AUTO_INCREMENT,
+    `title`
+    VARCHAR
+(
+    255
+) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `url` VARCHAR
+(
+    255
+) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `clasz` VARCHAR
+(
+    255
+) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `end_date` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY
+(
+    `id`
+)
+    ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE =utf8mb4_unicode_ci;
+
+INSERT INTO `event` (`id`, `title`, `url`, `clasz`, `start_date`, `end_date`)
+VALUES (1, 'Example 1', 'http://www.example.com', 'event-success', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (2, 'Example 2', 'http://www.example.com', 'event-important', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (3, 'Example 3', 'http://www.example.com', 'event-info', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
